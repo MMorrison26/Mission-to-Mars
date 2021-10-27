c270b7115720da532fa091a1f88b7fc506194c5f
MMorrison26 <87578449+MMorrison26@users.noreply.github.com>
2021-10-26 20:37:58 -0600

    Adding final files

--- app.py
+++ app.py
@@ -9,11 +9,13 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
+
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
+
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
@@ -21,4 +23,4 @@ def scrape():
    return redirect('/', code=302)
 
 if __name__ == "__main__":
-   app.run()
+   app.run() 
